dde948edfe68d13226523be47670c74218bc03b9
서용현 <syh05247@naver.com>
2023-09-14 19:28:20 +0900

    feat #6: 애노테이션 추가
    @AllArgsConstructor, @NoArgsConstructor access 속성 및 @Builder 추가

--- src/main/java/fastcampus/team7/Livable_officener/domain/Company.java
+++ src/main/java/fastcampus/team7/Livable_officener/domain/Company.java
@@ -1,20 +1,24 @@
 package fastcampus.team7.Livable_officener.domain;
 
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @Entity
 public class Company extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "building_id")
     private Building building;
 
     @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private String unit;
 
 }
