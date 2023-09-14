53ff9b0797ab8532be05b0af9abe61c53f220c33
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-14 18:09:47 +0900

    feat #14: 웹소켓 핸드셰이크 실제 상황에 맞게 수정
    - 메소드 추출 리팩토링

--- src/main/java/fastcampus/team7/Livable_officener/global/interceptor/CustomHandshakeInterceptor.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/interceptor/CustomHandshakeInterceptor.java
@@ -1,6 +1,8 @@
 package fastcampus.team7.Livable_officener.global.interceptor;
 
-import fastcampus.team7.Livable_officener.repository.TestRoomRepository;
+import fastcampus.team7.Livable_officener.domain.Room;
+import fastcampus.team7.Livable_officener.domain.User;
+import fastcampus.team7.Livable_officener.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -12,23 +14,36 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CustomHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
 
-    private final TestRoomRepository testRoomRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request,
                                    ServerHttpResponse response,
                                    WebSocketHandler wsHandler,
-                                   Map<String, Object> attributes) throws Exception {
-        String path = request.getURI().getPath();
-        String roomIdStr = path.substring(path.lastIndexOf('/') + 1);
-        Long roomId = Long.parseLong(roomIdStr);
+                                   Map<String, Object> attributes) {
+        Room room = getRoom(request);
+        User sender = getSender(request);
 
-        if (testRoomRepository.findById(roomId).isEmpty()) {
-            return false;
-        }
-
-        attributes.put("roomId", roomId);
+        attributes.put("room", room);
+        attributes.put("sender", sender);
 
         return true;
+    }
+
+    private Room getRoom(ServerHttpRequest request) {
+        Long roomId = getRoomId(request);
+        return chatRoomRepository.findById(roomId)
+                // TODO NotFoundRoomException 정의
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private User getSender(ServerHttpRequest request) {
+        return (User) request.getPrincipal();
+    }
+
+    private static Long getRoomId(ServerHttpRequest request) {
+        String path = request.getURI().getPath();
+        String roomIdStr = path.substring(path.lastIndexOf('/') + 1);
+        return Long.parseLong(roomIdStr);
     }
 }
