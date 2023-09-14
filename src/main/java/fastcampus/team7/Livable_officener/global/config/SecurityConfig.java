241f7805b7be39973bcf84ebcdf14b107ca5f318
서용현 <syh05247@naver.com>
2023-09-14 19:33:18 +0900

    feat #6: 시큐리티 설정
    * 기본적인 api 명세서에 따라 시큐리티를 설정하였음
    * cors 설정은 아직 하지 않은 상태

---
+++ src/main/java/fastcampus/team7/Livable_officener/global/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package fastcampus.team7.Livable_officener.global.config;
+
+import fastcampus.team7.Livable_officener.global.sercurity.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/h2-console/**", "/api/building", "/api/authentication", "/api/verification", "/api/signup", "/api/login")
+                    .permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .disable()
+                .csrf()
+                    .disable()
+                .httpBasic()
+                    .disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
