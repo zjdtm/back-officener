0c0053f2ff5f2cc2645521ca974902cc2e22e995
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-15 19:05:13 +0900

    feat #22: Room 참여마감 구현

--- src/main/java/fastcampus/team7/Livable_officener/domain/Room.java
+++ src/main/java/fastcampus/team7/Livable_officener/domain/Room.java
@@ -2,6 +2,7 @@ package fastcampus.team7.Livable_officener.domain;
 
 import fastcampus.team7.Livable_officener.global.constant.FoodTag;
 import fastcampus.team7.Livable_officener.global.constant.RoomStatus;
+import fastcampus.team7.Livable_officener.global.exception.NotActiveRoomException;
 import lombok.*;
 
 import javax.persistence.*;
@@ -52,4 +53,11 @@ public class Room extends BaseEntity {
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private RoomStatus status;
+
+    public void closeParticipation() {
+        if (getStatus() != RoomStatus.ACTIVE) {
+            throw new NotActiveRoomException();
+        }
+        status = RoomStatus.CLOSED;
+    }
 }
