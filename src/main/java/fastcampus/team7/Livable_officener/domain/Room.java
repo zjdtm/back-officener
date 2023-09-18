f85eaddd9c0e02d6fffa9fc2b7fe809130c14cdc
sdfgx123 <sdfgx123@naver.com>
2023-09-18 16:42:09 +0900

    feat #27: 함께배달 방, 참가자 엔티티 수정

--- src/main/java/fastcampus/team7/Livable_officener/domain/Room.java
+++ src/main/java/fastcampus/team7/Livable_officener/domain/Room.java
@@ -3,6 +3,7 @@ package fastcampus.team7.Livable_officener.domain;
 import fastcampus.team7.Livable_officener.global.constant.BankName;
 import fastcampus.team7.Livable_officener.global.constant.FoodTag;
 import fastcampus.team7.Livable_officener.global.constant.RoomStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Room extends BaseEntity {
 
--- src/main/java/fastcampus/team7/Livable_officener/domain/RoomParticipant.java
+++ src/main/java/fastcampus/team7/Livable_officener/domain/RoomParticipant.java
@@ -1,6 +1,7 @@
 package fastcampus.team7.Livable_officener.domain;
 
 import fastcampus.team7.Livable_officener.global.constant.Role;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @RequiredArgsConstructor
 @Entity
 public class RoomParticipant extends BaseEntity {
@@ -24,12 +26,6 @@ public class RoomParticipant extends BaseEntity {
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private Role role;
-
-    @Column
-    private LocalDateTime createdAt;
-
-    @Column
-    private LocalDateTime updatedAt;
 
     @Column
     private LocalDateTime kickedAt;
