eb14a463d4b6709274a32969b923731a1de371fb
Across826 <suwio0124@gmail.com>
2023-09-18 17:38:41 +0900

    test #30: 송금완료시 해당 채팅방의 참여자 여부 테스트

--- src/test/java/fastcampus/team7/Livable_officener/service/ChatServiceTransferCompleteTest.java
+++ src/test/java/fastcampus/team7/Livable_officener/service/ChatServiceTransferCompleteTest.java
@@ -1,11 +1,18 @@
 package fastcampus.team7.Livable_officener.service;
 
+import fastcampus.team7.Livable_officener.domain.Room;
+import fastcampus.team7.Livable_officener.domain.RoomParticipant;
 import fastcampus.team7.Livable_officener.domain.User;
+import fastcampus.team7.Livable_officener.global.constant.Role;
 import fastcampus.team7.Livable_officener.global.exception.NotFoundRoomException;
+import fastcampus.team7.Livable_officener.global.exception.UserIsNotGuestException;
+import fastcampus.team7.Livable_officener.global.exception.UserIsNotHostException;
+import fastcampus.team7.Livable_officener.global.exception.UserIsNotParticipantException;
 import fastcampus.team7.Livable_officener.repository.XChatRoomParticipantRepository;
 import fastcampus.team7.Livable_officener.repository.XChatRoomRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -42,6 +49,27 @@ public class ChatServiceTransferCompleteTest {
         assertThatThrownBy(() ->
                 sut.completeTransfer(WrongRoomId, user))
                 .isInstanceOf(NotFoundRoomException.class);
+    }
+
+    @Test
+    @DisplayName("송금완료 시 해당 채팅방의 참여자가 아니면 예외발생")
+    void isParticipantTest() {
+        // given
+        Long roomId = 1L;
+        User user = mock(User.class);
+        Room room = mock(Room.class);
+
+        given(user.getId())
+                .willReturn(1L);
+        given(roomRepository.findById(anyLong()))
+                .willReturn(Optional.of(room));
+        given(roomParticipantRepository.findByRoomIdAndUserId(anyLong(), anyLong()))
+                .willReturn(Optional.empty());
+
+        // when, then
+        assertThatThrownBy(() ->
+                sut.completeTransfer(roomId, user))
+                .isInstanceOf(UserIsNotParticipantException.class);
     }
 
 }
