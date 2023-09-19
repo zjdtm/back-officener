b54aad93d1c9cafa5c1c60e1a0ca0e5506e655dc
Ussu1112 <ussu1112@naver.com>
2023-09-19 14:09:26 +0900

    feat #47: 함깨배달 게시글 수정/삭제 API 구현

--- src/main/java/fastcampus/team7/Livable_officener/controller/DeliveryController.java
+++ src/main/java/fastcampus/team7/Livable_officener/controller/DeliveryController.java
@@ -1,28 +1,22 @@
 package fastcampus.team7.Livable_officener.controller;
 
 
+import fastcampus.team7.Livable_officener.domain.User;
+import fastcampus.team7.Livable_officener.dto.DeliveryRequestDTO;
 import fastcampus.team7.Livable_officener.dto.RoomDetailDTO;
+import fastcampus.team7.Livable_officener.dto.UpdateStoreDetailDTO;
+import fastcampus.team7.Livable_officener.global.util.APIDataResponse;
+import fastcampus.team7.Livable_officener.repository.BankRepository;
 import fastcampus.team7.Livable_officener.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import fastcampus.team7.Livable_officener.domain.Bank;
-import fastcampus.team7.Livable_officener.domain.User;
-import fastcampus.team7.Livable_officener.dto.DeliveryRequestDTO;
-import fastcampus.team7.Livable_officener.global.util.APIDataResponse;
-import fastcampus.team7.Livable_officener.repository.BankRepository;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -53,5 +47,24 @@ public class DeliveryController {
         deliveryService.registerRoom(createDTO, user);
 
         return APIDataResponse.of(HttpStatus.CREATED, "성공", "API 성공");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> modifyStoreDetail(
+            @PathVariable Long id,
+            @RequestBody UpdateStoreDetailDTO requestDTO,
+            @AuthenticationPrincipal User user) {
+        deliveryService.updateStoreDetail(id, requestDTO, user);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteDelivery(
+            @PathVariable Long id,
+            @AuthenticationPrincipal User user) {
+        deliveryService.deleteDelivery(id, user);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
