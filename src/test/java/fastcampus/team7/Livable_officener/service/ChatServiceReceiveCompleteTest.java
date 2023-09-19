d15e229bdd95d05b84be70b4e780ede72e395887
Across826 <suwio0124@gmail.com>
2023-09-19 18:23:12 +0900

    test #61: 이미 송금완료된 경우 예외발생 테스트

---
+++ src/test/java/fastcampus/team7/Livable_officener/service/ChatServiceReceiveCompleteTest.java
@@ -0,0 +1,65 @@
+package fastcampus.team7.Livable_officener.service;
+
+import fastcampus.team7.Livable_officener.domain.Room;
+import fastcampus.team7.Livable_officener.domain.RoomParticipant;
+import fastcampus.team7.Livable_officener.domain.User;
+import fastcampus.team7.Livable_officener.global.constant.Role;
+import fastcampus.team7.Livable_officener.global.exception.AlreadyReceivedException;
+import fastcampus.team7.Livable_officener.global.exception.AlreadyTransferredException;
+import fastcampus.team7.Livable_officener.repository.XChatRoomParticipantRepository;
+import fastcampus.team7.Livable_officener.repository.XChatRoomRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+public class ChatServiceReceiveCompleteTest {
+
+    @InjectMocks
+    private ChatService sut;
+    @Mock
+    private XChatRoomRepository roomRepository;
+    @Mock
+    private XChatRoomParticipantRepository roomParticipantRepository;
+
+    @Test
+    @DisplayName("이미 송금완료 했을 시 예외 발생")
+    void alreadyTransfer() {
+        //given
+        Long roomId = 1L;
+        User user = mock(User.class);
+        Room room = mock(Room.class);
+        RoomParticipant roomParticipant = mock(RoomParticipant.class);
+
+        given(user.getId())
+                .willReturn(1L);
+        given(roomRepository.findById(anyLong()))
+                .willReturn(Optional.of(room));
+        given(roomParticipantRepository.findByRoomIdAndUserId(anyLong(), anyLong()))
+                .willReturn(Optional.of(roomParticipant));
+        given(roomParticipant.getRole())
+                .willReturn(Role.GUEST);
+        doThrow(AlreadyReceivedException.class)
+                .when(roomParticipant).completeReceive();
+
+        customAssertThrow(roomId, user, AlreadyReceivedException.class);
+    }
+
+    void customAssertThrow(Long roomId, User user,
+                           Class<? extends Throwable> ex) {
+        assertThatThrownBy(() ->
+                sut.completeReceive(roomId, user))
+                .isInstanceOf(ex);
+    }
+}
