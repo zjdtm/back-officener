93bb16ce4300862d818d229e6b484848454cc385
KH <kkh96071@gmail.com>
2023-09-19 18:26:58 +0900

    feature/#54-setElevator

--- src/main/java/fastcampus/team7/Livable_officener/domain/UserElevator.java
+++ src/main/java/fastcampus/team7/Livable_officener/domain/UserElevator.java
@@ -3,17 +3,18 @@ package fastcampus.team7.Livable_officener.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+
 
 @Entity
 @Getter
 @Setter
 public class UserElevator extends BaseEntity{
 
+    @Column(nullable = false)
     private Long ElevatorId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
+    @Column(nullable = false)
+    private Long userId;
 }
