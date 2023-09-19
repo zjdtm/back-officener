2e445357869fbb26a199155b2f87adc106064d39
KH <kkh96071@gmail.com>
2023-09-19 13:22:05 +0900

    refactor - 엘리베이터 리스트 수정

--- src/main/java/fastcampus/team7/Livable_officener/controller/ElevatorController.java
+++ src/main/java/fastcampus/team7/Livable_officener/controller/ElevatorController.java
@@ -1,12 +1,15 @@
 package fastcampus.team7.Livable_officener.controller;
 
 import fastcampus.team7.Livable_officener.dto.ElevatorDTO;
+import fastcampus.team7.Livable_officener.global.sercurity.JwtProvider;
 import fastcampus.team7.Livable_officener.global.util.APIDataResponse;
 import fastcampus.team7.Livable_officener.repository.ElevatorRepository;
 import fastcampus.team7.Livable_officener.service.ElevatorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,8 +22,12 @@ public class ElevatorController {
 
     private final ElevatorService elevatorService;
 
+    private final JwtProvider jwtProvider;
+
     @GetMapping("/api/elevator")
-    public ResponseEntity<APIDataResponse<List<ElevatorDTO>>> getAllElevators(){
+    public ResponseEntity<APIDataResponse<List<ElevatorDTO>>> getAllElevators(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization){
+
         return elevatorService.getAllElevators();
     }
 
