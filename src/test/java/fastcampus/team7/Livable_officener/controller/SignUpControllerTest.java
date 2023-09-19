ab8d3c6822d669a8b63d3ff1dfc43765da6616d9
서용현 <syh05247@naver.com>
2023-09-19 21:30:34 +0900

    test #46: 회원가입시 인증번호 요청 성공, 검증 테스트 코드 추가

--- src/test/java/fastcampus/team7/Livable_officener/controller/SignUpControllerTest.java
+++ src/test/java/fastcampus/team7/Livable_officener/controller/SignUpControllerTest.java
@@ -1,7 +1,10 @@
 package fastcampus.team7.Livable_officener.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fastcampus.team7.Livable_officener.dto.BuildingWithCompaniesDTO;
 import fastcampus.team7.Livable_officener.dto.CompanyDTO;
+import fastcampus.team7.Livable_officener.dto.PhoneAuthRequestDTO;
+import fastcampus.team7.Livable_officener.global.exception.DuplicatedPhoneNumberException;
 import fastcampus.team7.Livable_officener.service.SignUpService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,8 +22,12 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,8 +38,12 @@ class SignUpControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private SignUpService signUpService;
+
 
     @Test
     @WithMockUser
@@ -101,5 +113,50 @@ class SignUpControllerTest {
         mvc.perform(get("/api/building").param("name", keyword))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data", hasSize(0)));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("휴대폰 번호 인증 요청시 적절한 값이 출력되는지 테스트")
+    void givenNameAndPhoneNumber_whenPost_thenReturnVerifyCode() throws Exception {
+
+        // given
+        final String verifyCode = "864583";
+        PhoneAuthRequestDTO phoneAuthRequestDTO = PhoneAuthRequestDTO.builder()
+                .name("고길동")
+                .phoneNumber("01012345678")
+                .build();
+
+        given(signUpService.getPhoneAuthCode(any(PhoneAuthRequestDTO.class))).willReturn(verifyCode);
+
+        // when & then
+        mvc.perform(post("/api/auth")
+                        .with(csrf())
+                        .content(objectMapper.writeValueAsString(phoneAuthRequestDTO))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", is(verifyCode)));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("휴대폰 번호 인증 요청시 이미 등록된 핸드폰 번호 일 경우 예외 발생")
+    void givenNameAndPhoneNumber_whenPost_thenDuplicatedPhoneNumber() throws Exception {
+
+        // given
+        final String verifyCode = "864583";
+        PhoneAuthRequestDTO phoneAuthRequestDTO = PhoneAuthRequestDTO.builder()
+                .name("고길동")
+                .phoneNumber("01012345678")
+                .build();
+
+        given(signUpService.getPhoneAuthCode(any(PhoneAuthRequestDTO.class))).willThrow(DuplicatedPhoneNumberException.class);
+
+        // when & then
+        mvc.perform(post("/api/auth")
+                        .with(csrf())
+                        .content(objectMapper.writeValueAsString(phoneAuthRequestDTO))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 }
