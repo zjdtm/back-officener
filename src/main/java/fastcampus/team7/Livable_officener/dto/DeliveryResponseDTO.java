46cf0f3e4e007993226491224ab83844b2a30091
sdfgx123 <sdfgx123@naver.com>
2023-09-19 13:07:50 +0900

    feat #31: 함께배달 리스트 관련 DTO 세팅

--- src/main/java/fastcampus/team7/Livable_officener/dto/DeliveryResponseDTO.java
+++ src/main/java/fastcampus/team7/Livable_officener/dto/DeliveryResponseDTO.java
@@ -1,4 +1,36 @@
 package fastcampus.team7.Livable_officener.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class DeliveryResponseDTO {
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter @Setter
+    @Builder
+    public static class RoomListResponseDTO {
+        private Long roomId;
+        private Long hostId;
+        private String storeName;
+        private String menuLink;
+        private Long deliveryFee;
+        private String tag;
+        private Long attendees;
+        private Long maxAttendees;
+        private LocalDateTime deadLine;
+        private String roomStatus;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+    }
+
+    @Getter @Setter
+    public static class PagedRoomListResponseDTO {
+        private int currentPage;
+        private int totalPage;
+        private Long totalElements;
+        private List<RoomListResponseDTO> rooms;
+    }
 }
