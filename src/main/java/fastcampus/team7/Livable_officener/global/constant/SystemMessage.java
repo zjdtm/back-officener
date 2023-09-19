89413b8d88e11670faa7b92ddeb601be035c5b20
Across826 <suwio0124@gmail.com>
2023-09-19 18:09:44 +0900

    feat #61: 수령 완료 메세지 작성

--- src/main/java/fastcampus/team7/Livable_officener/global/constant/SystemMessage.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/constant/SystemMessage.java
@@ -2,7 +2,8 @@ package fastcampus.team7.Livable_officener.global.constant;
 
 public enum SystemMessage {
     CLOSE_PARTICIPATION("%s님이 참여한 배달의 모집이 완료되었습니다."),
-    COMPLETE_TRANSFER("%s님이 송금을 완료했어요! 호스트님 확인해주세요.")
+    COMPLETE_TRANSFER("%s님이 송금을 완료했어요! 호스트님 확인해주세요."),
+    COMPLETE_RECEIVE("%s님이 수령을 완료했어요!"),
     ;
 
     private final String contentFormat;
