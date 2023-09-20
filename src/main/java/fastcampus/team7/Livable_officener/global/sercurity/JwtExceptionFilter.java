f4bc0da2617f107f804a65869328112ef9839f8b
서용현 <syh05247@naver.com>
2023-09-21 03:32:41 +0900

    refactor #23: Jwt 에러 처리 추가
    * Jwt 에러 발생시 response 에 메시지를 담는 형태로 작성
    (추 후에 응답값에 맞게 형식을 바꾸어야 함)

--- src/main/java/fastcampus/team7/Livable_officener/global/sercurity/JwtExceptionFilter.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/sercurity/JwtExceptionFilter.java
@@ -1,7 +1,8 @@
 package fastcampus.team7.Livable_officener.global.sercurity;
 
+import fastcampus.team7.Livable_officener.global.constant.JwtExceptionCode;
 import io.jsonwebtoken.JwtException;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,41 +14,46 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtExceptionFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
         try {
             filterChain.doFilter(request, response);
         } catch (JwtException e) {
+            log.info("JwtExceptionFilter 예외 발생");
+
             String message = e.getMessage();
-            if (message.equals("시그니처 에러입니다.")) {
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.getWriter().write("시그니처 에러입니다.");
+
+            // TODO : 응답 값에 맞게 DTO 형태로 바꿔줘야 함
+            if (JwtExceptionCode.UNKNOWN_ERROR.getMessage().equals(message)) {
+                setResponse(response, JwtExceptionCode.UNKNOWN_ERROR);
             }
             //잘못된 타입의 토큰인 경우
-            else if (message.equals("멜폼 에러입니다.")) {
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.getWriter().write("멜폼 에러입니다.");
+            else if (JwtExceptionCode.WRONG_TYPE_TOKEN.getMessage().equals(message)) {
+                setResponse(response, JwtExceptionCode.WRONG_TYPE_TOKEN);
             }
             //토큰 만료된 경우
-            else if (message.equals("익스파이어 에러입니다.")) {
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.getWriter().write("익스파이어 에러입니다.");
+            else if (JwtExceptionCode.EXPIRED_TOKEN.getMessage().equals(message)) {
+                setResponse(response, JwtExceptionCode.EXPIRED_TOKEN);
             }
             //지원되지 않는 토큰인 경우
-            else if (message.equals("일리걸아규먼트 에러입니다.")) {
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.getWriter().write("일리걸아규먼트 에러입니다.");
+            else if (JwtExceptionCode.UNSUPPORTED_TOKEN.getMessage().equals(message)) {
+                setResponse(response, JwtExceptionCode.UNSUPPORTED_TOKEN);
             } else {
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.getWriter().write("알 수 없는 에러입니다.");
+                setResponse(response, JwtExceptionCode.UNKNOWN_ERROR);
             }
         }
+
+    }
+
+    private void setResponse(HttpServletResponse response, JwtExceptionCode errorMessage) throws RuntimeException, IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setCharacterEncoding("utf-8");
+
+        response.getWriter().print(errorMessage.getMessage());
     }
 }
