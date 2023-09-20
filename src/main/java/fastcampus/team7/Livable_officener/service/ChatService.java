085dcee7679839129ad5992109741a6934fec457
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-21 01:48:21 +0900

    fix #75: ChatType, SystemMessage 통합
    - ChatType으로 통합합니다.
    - ChatType을 동사구로 바꿉니다.
    - 웹소켓 메시지 송신 방식을 수정합니다. (커밋 꼬임..)

--- src/main/java/fastcampus/team7/Livable_officener/service/ChatService.java
+++ src/main/java/fastcampus/team7/Livable_officener/service/ChatService.java
@@ -1,13 +1,16 @@
 package fastcampus.team7.Livable_officener.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import fastcampus.team7.Livable_officener.domain.Chat;
 import fastcampus.team7.Livable_officener.domain.Room;
 import fastcampus.team7.Livable_officener.domain.RoomParticipant;
 import fastcampus.team7.Livable_officener.domain.User;
-import fastcampus.team7.Livable_officener.dto.SendChatDTO;
+import fastcampus.team7.Livable_officener.dto.chat.SendChatDTO;
+import fastcampus.team7.Livable_officener.dto.chat.SendPayloadDTO;
 import fastcampus.team7.Livable_officener.global.constant.ChatType;
 import fastcampus.team7.Livable_officener.global.constant.Role;
-import fastcampus.team7.Livable_officener.global.constant.SystemMessage;
 import fastcampus.team7.Livable_officener.global.exception.*;
 import fastcampus.team7.Livable_officener.global.websocket.WebSocketSessionManager;
 import fastcampus.team7.Livable_officener.repository.ChatRepository;
@@ -17,14 +20,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
 public class ChatService {
+
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 
     private final ChatRepository chatRepository;
     private final DeliveryRepository roomRepository;
@@ -32,11 +41,21 @@ public class ChatService {
     private final WebSocketSessionManager webSocketSessionManager;
 
     @Transactional
-    public void send(SendChatDTO dto) throws IOException {
-        for (WebSocketSession connectedSession : dto.getWebSocketSessions()) {
-            connectedSession.sendMessage(dto.getMessage());
-        }
-        chatRepository.save(Chat.from(dto));
+    public void send(SendChatDTO sendChatDTO) throws IOException {
+        Room room = sendChatDTO.getRoom();
+        User sender = sendChatDTO.getSender();
+        SendPayloadDTO payloadDto = getSendPayloadDTO(sender, sendChatDTO.getMessage());
+
+        sendMessage(room, sender, payloadDto);
+    }
+
+    private SendPayloadDTO getSendPayloadDTO(User sender, TextMessage message) throws JsonProcessingException {
+        Long senderId = sender.getId();
+        String sendPayload = message.getPayload();
+
+        SendPayloadDTO sendPayloadDTO = objectMapper.readValue(sendPayload, SendPayloadDTO.class);
+        sendPayloadDTO.setSenderId(senderId);
+        return sendPayloadDTO;
     }
 
     @Transactional
@@ -47,19 +66,20 @@ public class ChatService {
         validateIfRoomParticipantIsHost(roomParticipant.getRole(), "참여마감하기");
 
         room.closeParticipation();
-        sendSystemMessage(room, user, SystemMessage.CLOSE_PARTICIPATION);
+
+        sendSystemMessage(room, user, ChatType.CLOSE_PARTICIPATION);
     }
 
     @Transactional
     public void completeRemit(Long roomId, User user) throws IOException {
         Room room = getRoom(roomId);
-        RoomParticipant roomParticipant = getRoomParticipant(roomId, user.getId());
 
+        RoomParticipant roomParticipant = getRoomParticipant(roomId, user.getId());
         validateIfRoomParticipantIsGuest(roomParticipant.getRole(), "송금완료");
         isRemitCompleted(roomParticipant);
-
         roomParticipant.completeRemit();
-        sendSystemMessage(room, user, SystemMessage.COMPLETE_REMIT);
+
+        sendSystemMessage(room, user, ChatType.COMPLETE_REMITTANCE);
     }
 
     @Transactional
@@ -71,7 +91,7 @@ public class ChatService {
 
         room.completeDelivery();
 
-        sendSystemMessage(room, user, SystemMessage.COMPLETE_DELIVERY);
+        sendSystemMessage(room, user, ChatType.COMPLETE_DELIVERY);
     }
 
     @Transactional
@@ -81,16 +101,17 @@ public class ChatService {
         validateIfRoomParticipantIsGuest(roomParticipant.getRole(), "수령완료");
         isReceiveCompleted(roomParticipant);
         roomParticipant.completeReceive();
-        sendSystemMessage(room, user, SystemMessage.COMPLETE_RECEIVE);
+
+        sendSystemMessage(room, user, ChatType.COMPLETE_RECEIPT);
     }
 
     @Transactional
-    public void kickRequest(Long roomId, User user) throws IOException{
-        Room room = roomRepository.findById(roomId)
-                .orElseThrow(() -> new NotFoundRoomException());
+    public void kickRequest(Long roomId, User user) throws IOException {
+        Room room = getRoom(roomId);
         RoomParticipant roomParticipant = getRoomParticipant(room.getId(), user.getId());
         validateIfRoomParticipantIsGuest(roomParticipant.getRole(), "나가기요청");
-        sendSystemMessage(room,user,SystemMessage.EXIT_REQUEST);
+
+        sendSystemMessage(room, user, ChatType.REQUEST_EXIT);
     }
 
     private Room getRoom(Long roomId) {
@@ -127,25 +148,47 @@ public class ChatService {
         }
     }
 
+    private void sendSystemMessage(Room room, User sender, ChatType messageType) throws IOException {
+        SendPayloadDTO payloadDto = createSystemMessagePayloadDTO(sender, messageType);
 
-    private void sendSystemMessage(Room room, User user, SystemMessage systemMessage) throws IOException {
-        Collection<WebSocketSession> webSocketSessions = webSocketSessionManager.getWebSocketSessions(room.getId());
-        TextMessage textMessage = getTextMessage(systemMessage, user);
-        send(new SendChatDTO(room, user, textMessage, ChatType.SYSTEM_MESSAGE, webSocketSessions));
+        sendMessage(room, sender, payloadDto);
     }
 
-    private static TextMessage getTextMessage(SystemMessage systemMessage, User user) {
-        Object[] systemMessageArgs = getSystemMessageArgs(systemMessage, user);
-        return new TextMessage(systemMessage.getContent(systemMessageArgs));
+    private static SendPayloadDTO createSystemMessagePayloadDTO(User sender, ChatType messageType) {
+        String content = getSystemMessageContent(sender, messageType);
+        return createSystemMessagePayloadDTO(sender, messageType, content);
     }
 
-    private static Object[] getSystemMessageArgs(SystemMessage systemMessage, User user) {
+    private static String getSystemMessageContent(User sender, ChatType messageType) {
+        Object[] systemMessageArgs = getSystemMessageArgs(messageType, sender);
+        return messageType.getSystemMessageContent(systemMessageArgs);
+    }
+
+    private static Object[] getSystemMessageArgs(ChatType messageType, User user) {
         Object[] systemMessageArgs;
-        if (systemMessage == SystemMessage.COMPLETE_DELIVERY) {
+        if (messageType == ChatType.COMPLETE_DELIVERY) {
             systemMessageArgs = null;
         } else {
             systemMessageArgs = new Object[]{user.getName()};
         }
         return systemMessageArgs;
+    }
+
+    private static SendPayloadDTO createSystemMessagePayloadDTO(User sender, ChatType messageType, String content) {
+        SendPayloadDTO systemMessagePayloadDTO = new SendPayloadDTO(messageType, content, LocalDateTime.now());
+        systemMessagePayloadDTO.setSenderId(sender.getId());
+        return systemMessagePayloadDTO;
+    }
+
+    private void sendMessage(Room room, User sender, SendPayloadDTO payloadDto) throws IOException {
+        TextMessage message = convertPayloadDtoToJsonTextMessage(payloadDto);
+
+        webSocketSessionManager.send(room.getId(), message);
+        chatRepository.save(Chat.from(room, sender, payloadDto));
+    }
+
+    private TextMessage convertPayloadDtoToJsonTextMessage(SendPayloadDTO payloadDTO) throws JsonProcessingException {
+        String payload = objectMapper.writeValueAsString(payloadDTO);
+        return new TextMessage(payload);
     }
 }
