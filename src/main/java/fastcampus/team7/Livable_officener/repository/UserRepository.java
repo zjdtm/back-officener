258bab5674ff2269faa4c12205fab8b32f2a29d9
서용현 <syh05247@naver.com>
2023-09-20 21:51:30 +0900

    feat #23: existsByEmail 쿼리 메서드 추가

--- src/main/java/fastcampus/team7/Livable_officener/repository/UserRepository.java
+++ src/main/java/fastcampus/team7/Livable_officener/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByPhoneNumber(String phoneNumber);
+
+    boolean existsByEmail(String email);
 }
