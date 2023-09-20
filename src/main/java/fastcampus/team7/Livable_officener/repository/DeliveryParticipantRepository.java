701765519d132d56bbc80044cde5d5be5e703d66
Across826 <suwio0124@gmail.com>
2023-09-20 20:43:19 +0900

    feat #64: 참가자 리스트 조회

--- src/main/java/fastcampus/team7/Livable_officener/repository/DeliveryParticipantRepository.java
+++ src/main/java/fastcampus/team7/Livable_officener/repository/DeliveryParticipantRepository.java
@@ -3,7 +3,11 @@ package fastcampus.team7.Livable_officener.repository;
 import fastcampus.team7.Livable_officener.domain.RoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DeliveryParticipantRepository extends JpaRepository<RoomParticipant, Long>, DeliveryParticipantRepositoryCustom {
 
     void deleteAllByRoomId(Long roomId);
+
+    List<RoomParticipant> findAllByRoomId(Long roomId);
 }
