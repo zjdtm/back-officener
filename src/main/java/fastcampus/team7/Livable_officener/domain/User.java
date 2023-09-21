9cb8831290912faccdc1df9ee89db1bfc87c0457
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-21 22:08:11 +0900

    fix #103: User가 UserDetails를 구현
    - CustomUserDetails 삭제
    - CustomUserDetailsService에서 User를 리턴하도록 변경
    - NotFoundUserException 메시지 생성 방법 수정

--- src/main/java/fastcampus/team7/Livable_officener/domain/User.java
+++ src/main/java/fastcampus/team7/Livable_officener/domain/User.java
@@ -1,16 +1,21 @@
 package fastcampus.team7.Livable_officener.domain;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @Entity
 @Builder
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "member")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Building building;
@@ -31,4 +36,39 @@ public class User extends BaseEntity {
     private String phoneNumber;
 
     private String profileImage;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("일반 회원"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/fastcampus/team7/Livable_officener/global/exception/NotFoundUserException.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/exception/NotFoundUserException.java
@@ -5,11 +5,11 @@ public class NotFoundUserException extends RuntimeException{
     public static final String DEFAULT_MESSAGE = "존재하지 않는 회원입니다.";
 
     public NotFoundUserException() {
-        this(DEFAULT_MESSAGE);
+        super(DEFAULT_MESSAGE);
     }
 
-    public NotFoundUserException(String s) {
-        super(s);
+    public NotFoundUserException(String username) {
+        super(DEFAULT_MESSAGE + " username: " + username);
     }
 
 }
