818080048ae53e4b95bfcaa0a58c97fc0b59344f
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-23 23:26:27 +0900

    style #115: 시큐리티 설정 람다 스타일로 변경

--- src/main/java/fastcampus/team7/Livable_officener/global/config/SecurityConfig.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package fastcampus.team7.Livable_officener.global.config;
 
-import fastcampus.team7.Livable_officener.global.sercurity.*;
-import io.jsonwebtoken.JwtException;
+import fastcampus.team7.Livable_officener.global.sercurity.JwtAuthenticationFilter;
+import fastcampus.team7.Livable_officener.global.sercurity.JwtExceptionFilter;
+import fastcampus.team7.Livable_officener.global.sercurity.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,20 +27,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/**") // 모든 경로에 대해
-                .permitAll()
-                .and()
-                .formLogin()
-                    .disable()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class)
-                .csrf()
-                    .disable()
-                .httpBasic()
-                    .disable()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            .authorizeRequests(authorizationRequest -> authorizationRequest
+                    .antMatchers("/**")
+                    .permitAll())
+            .formLogin(AbstractHttpConfigurer::disable)
+            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class)
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .sessionManagement(sessionManagement -> sessionManagement
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
