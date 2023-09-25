2006ac13cf211322b517746edee53c16cbce43c5
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-25 13:48:35 +0900

    test #97: 송신 및 저장되는 Chat에 senderId 포함

--- src/test/java/fastcampus/team7/Livable_officener/service/ChatServiceSendTest.java
+++ src/test/java/fastcampus/team7/Livable_officener/service/ChatServiceSendTest.java
@@ -4,26 +4,51 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import fastcampus.team7.Livable_officener.domain.Chat;
+import fastcampus.team7.Livable_officener.domain.Room;
+import fastcampus.team7.Livable_officener.domain.User;
+import fastcampus.team7.Livable_officener.dto.chat.SendChatDTO;
 import fastcampus.team7.Livable_officener.dto.chat.SendPayloadDTO;
 import fastcampus.team7.Livable_officener.global.constant.ChatType;
 import fastcampus.team7.Livable_officener.global.util.LocalDateTimeDeserializer;
+import fastcampus.team7.Livable_officener.global.websocket.WebSocketSessionManager;
+import fastcampus.team7.Livable_officener.repository.ChatRepository;
+import fastcampus.team7.Livable_officener.repository.DeliveryParticipantRepository;
+import fastcampus.team7.Livable_officener.repository.DeliveryRepository;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.socket.TextMessage;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ChatServiceSendTest {
 
-    private static final ObjectMapper mapper;
+    private final static ObjectMapper mapper;
+
+    private static ChatService sut;
+    @Mock
+    private static ChatRepository chatRepository;
+    @Mock
+    private static DeliveryRepository roomRepository;
+    @Mock
+    private static DeliveryParticipantRepository roomParticipantRepository;
+    @Mock
+    private static WebSocketSessionManager webSocketSessionManager;
 
     static {
         mapper = new ObjectMapper();
@@ -34,6 +59,13 @@ class ChatServiceSendTest {
         mapper.registerModule(module);
         mapper.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        sut = new ChatService(mapper, chatRepository,
+                roomRepository, roomParticipantRepository,
+                webSocketSessionManager);
     }
 
     @DisplayName("mapper UTC -> Local")
@@ -64,5 +96,42 @@ class ChatServiceSendTest {
 
         String payloadWritten = mapper.writeValueAsString(payloadDto);
         System.out.println("payloadWritten = " + payloadWritten);
+    }
+
+    @DisplayName("송신 및 저장되는 Chat에 senderId 포함")
+    @Test
+    void givenMessageWithoutSender_thenChatHasSender() throws IOException {
+        // given
+        Long roomId = 1L;
+        Room room = mock(Room.class);
+        given(room.getId()).willReturn(roomId);
+        User sender = User.builder()
+                .id(1L)
+                .name("정준희")
+                .email("joonhee@example.com")
+                .build();
+        Map<String, Object> map = new HashMap<>();
+        final ChatType messageType = ChatType.TALK;
+        final String content = "테스트 메시지";
+        final String sendTime = "2023-09-22T10:08:24.395Z";
+        final String sendTimeLocal = "2023-09-22T19:08:24.395";
+        map.put("messageType", messageType.name());
+        map.put("content", content);
+        map.put("sendTime", sendTime);
+        String payload = new JSONObject(map).toString();
+        TextMessage message = new TextMessage(payload);
+        SendChatDTO sendChatDTO = new SendChatDTO(room, sender, message);
+
+        // when
+        sut.send(sendChatDTO);
+
+        // then
+        ArgumentCaptor<Chat> chatCaptor = ArgumentCaptor.forClass(Chat.class);
+        verify(chatRepository, times(1)).save(chatCaptor.capture());
+        Chat chat = chatCaptor.getValue();
+        assertThat(chat.getType()).isEqualTo(messageType);
+        assertThat(chat.getContent()).isEqualTo(content);
+        assertThat(chat.getCreatedAt()).isEqualTo(sendTimeLocal);
+        assertThat(chat.getSender()).isEqualTo(sender);
     }
 }
