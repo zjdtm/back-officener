3f57625c5b1d302891f892c5c684bcb5b25710ce
서용현 <syh05247@naver.com>
2023-10-01 15:56:06 +0900

    feat #127: 로그아웃 예외 처리 추가
    * 로그아웃한 상태에 jwt 로 요청할 경우 예외처리를 추가하였음

--- src/main/java/fastcampus/team7/Livable_officener/global/sercurity/JwtExceptionFilter.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/sercurity/JwtExceptionFilter.java
@@ -1,8 +1,11 @@
 package fastcampus.team7.Livable_officener.global.sercurity;
 
-import fastcampus.team7.Livable_officener.global.constant.JwtExceptionCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fastcampus.team7.Livable_officener.global.constant.FilterExceptionCode;
+import fastcampus.team7.Livable_officener.global.exception.AlreadyLogoutException;
 import io.jsonwebtoken.JwtException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -12,10 +15,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
 public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -23,37 +31,44 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
         try {
             filterChain.doFilter(request, response);
         } catch (JwtException e) {
-            log.info("JwtExceptionFilter 예외 발생");
 
             String message = e.getMessage();
 
-            // TODO : 응답 값에 맞게 DTO 형태로 바꿔줘야 함
-            if (JwtExceptionCode.UNKNOWN_ERROR.getMessage().equals(message)) {
-                setResponse(response, JwtExceptionCode.UNKNOWN_ERROR);
+            // 알 수 없는 에러입니다.
+            if (FilterExceptionCode.UNKNOWN_ERROR.getMessage().equals(message)) {
+                setResponse(response, FilterExceptionCode.UNKNOWN_ERROR);
             }
-            //잘못된 타입의 토큰인 경우
-            else if (JwtExceptionCode.WRONG_TYPE_TOKEN.getMessage().equals(message)) {
-                setResponse(response, JwtExceptionCode.WRONG_TYPE_TOKEN);
+            // 잘못된 타입의 토큰인 경우
+            else if (FilterExceptionCode.WRONG_TYPE_TOKEN.getMessage().equals(message)) {
+                setResponse(response, FilterExceptionCode.WRONG_TYPE_TOKEN);
             }
-            //토큰 만료된 경우
-            else if (JwtExceptionCode.EXPIRED_TOKEN.getMessage().equals(message)) {
-                setResponse(response, JwtExceptionCode.EXPIRED_TOKEN);
+            // 토큰 만료된 경우
+            else if (FilterExceptionCode.EXPIRED_TOKEN.getMessage().equals(message)) {
+                setResponse(response, FilterExceptionCode.EXPIRED_TOKEN);
             }
-            //지원되지 않는 토큰인 경우
-            else if (JwtExceptionCode.UNSUPPORTED_TOKEN.getMessage().equals(message)) {
-                setResponse(response, JwtExceptionCode.UNSUPPORTED_TOKEN);
+            // 지원되지 않는 토큰인 경우
+            else if (FilterExceptionCode.UNSUPPORTED_TOKEN.getMessage().equals(message)) {
+                setResponse(response, FilterExceptionCode.UNSUPPORTED_TOKEN);
             } else {
-                setResponse(response, JwtExceptionCode.UNKNOWN_ERROR);
+                setResponse(response, FilterExceptionCode.UNKNOWN_ERROR);
             }
+        } catch (AlreadyLogoutException e) {
+            setResponse(response, FilterExceptionCode.ALREADY_LOGGED_OUT);
         }
 
     }
 
-    private void setResponse(HttpServletResponse response, JwtExceptionCode errorMessage) throws RuntimeException, IOException {
+    private void setResponse(HttpServletResponse response, FilterExceptionCode error) throws RuntimeException, IOException {
+
+        log.info("[JwtExceptionFilter] 예외 발생 - " + error.getMessage());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setCharacterEncoding("utf-8");
 
-        response.getWriter().print(errorMessage.getMessage());
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("errorMessage", error.getMessage());
+        String result = objectMapper.writeValueAsString(errorResponse);
+
+        response.getWriter().print(result);
     }
 }
