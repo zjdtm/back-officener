47016249ab10e751bb59ab73917014df46a691da
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-02 20:38:14 +0900

    feat #148: 웹소켓 설정에 핸드셰이크 핸들러 추가

--- src/main/java/fastcampus/team7/Livable_officener/global/websocket/WebSocketConfig.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/websocket/WebSocketConfig.java
@@ -13,12 +13,14 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     private final CustomTextWebSocketHandler customTextWebSocketHandler;
     private final CustomHandshakeInterceptor customHandshakeInterceptor;
+    private final CustomHandshakeHandler customHandshakeHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(customTextWebSocketHandler, "/api/chat/*")
                 .setAllowedOrigins("*")
                 .addInterceptors(customHandshakeInterceptor)
+                .setHandshakeHandler(customHandshakeHandler)
 //                .setAllowedOrigins("FRONT_ORIGIN") // 추후 프론트엔드 URL 결정되면 주석 풀기
 
         ;
