c0446b21f318085f1d56b0ad40df69faf4a03cec
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-05 01:22:15 +0900

    feat #125: 알림 내용 상수에 수령완료, 나가기요청 추가

--- src/main/java/fastcampus/team7/Livable_officener/global/constant/NotificationContent.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/constant/NotificationContent.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 public enum NotificationContent {
     DELIVERED("배달이 완료되었습니다. 찾아가주세요"),
     TRANSFERRED("송금완료이 완료되었습니다. 확인해주세요"),
+    COMPLETE_RECEIPT("누군가 수령을 완료했습니다."),
+    REQUEST_EXIT("누군가 나가기 요청을 했습니다."),
     KICK("강퇴당했습니다. 확인해주세요.")
     ;
 
