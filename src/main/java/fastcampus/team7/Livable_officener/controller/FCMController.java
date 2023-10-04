a4859cf6da42621f43cce61dbfe7e3807312ed8b
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-05 01:56:53 +0900

    feat #125: 알림 끄기 API 컨트롤러 구현

--- src/main/java/fastcampus/team7/Livable_officener/controller/FCMController.java
+++ src/main/java/fastcampus/team7/Livable_officener/controller/FCMController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/notify")
@@ -28,5 +25,14 @@ public class FCMController {
         fcmService.registerFcmToken(dto);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/fcm-token")
+    public ResponseEntity<?> unsubscribe(
+            @AuthenticationPrincipal User user) {
+
+        fcmService.unsubscribe(user.getId());
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
