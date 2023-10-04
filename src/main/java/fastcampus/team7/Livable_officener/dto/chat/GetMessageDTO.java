7d861e53d7d7e6701ed3b67e7525d781c21da864
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-04 14:54:21 +0900

    fix #165: 채팅방 정보 요청 시 content 포함
    - 버전 변경 포함

--- src/main/java/fastcampus/team7/Livable_officener/dto/chat/GetMessageDTO.java
+++ src/main/java/fastcampus/team7/Livable_officener/dto/chat/GetMessageDTO.java
@@ -1,6 +1,8 @@
 package fastcampus.team7.Livable_officener.dto.chat;
 
 import fastcampus.team7.Livable_officener.domain.Chat;
+import fastcampus.team7.Livable_officener.domain.User;
+import fastcampus.team7.Livable_officener.global.constant.ChatType;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,14 +17,22 @@ public class GetMessageDTO {
 
     private final Long messageId;
     private final String messageType;
+    private final String content;
     private final Long senderId;
     private final LocalDateTime sendTime;
 
     public static GetMessageDTO from(Chat chat) {
+        final ChatType messageType = chat.getType();
+        final User sender = chat.getSender();
+        String content = chat.getContent();
+        if (content == null) {
+            content = messageType.getSystemMessageContent(sender);
+        }
         return GetMessageDTO.builder()
                 .messageId(chat.getId())
-                .messageType(chat.getType().name())
-                .senderId(chat.getSender().getId())
+                .messageType(messageType.name())
+                .content(content)
+                .senderId(sender.getId())
                 .sendTime(chat.getCreatedAt())
                 .build();
     }
