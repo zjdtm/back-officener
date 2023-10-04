642a0ab691e2405f6a5d1c2c98e8160302dacafa
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-05 01:22:49 +0900

    feat #125: 알림 타입 상수에 수령완료, 나가기요청 추가

--- src/main/java/fastcampus/team7/Livable_officener/global/constant/NotificationType.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/constant/NotificationType.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 public enum NotificationType {
     DELIVERED("배달완료"),
     TRANSFERRED("송금완료"),
+    COMPLETE_RECEIPT("수령완료"),
+    REQUEST_EXIT("나가기요청"),
     KICK("강퇴")
 
     ;
