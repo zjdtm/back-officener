ac08175df9119f9670ba09d0e73d0d351b91a5ec
서용현 <syh05247@naver.com>
2023-10-05 22:21:50 +0900

    remove #218: 사용하지 않는 클래스 삭제

--- src/main/java/fastcampus/team7/Livable_officener/repository/PhoneAuthDTORedisRepository.java
+++
@@ -1,7 +0,0 @@
-package fastcampus.team7.Livable_officener.repository;
-
-import fastcampus.team7.Livable_officener.dto.signup.PhoneAuthDTO;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PhoneAuthDTORedisRepository extends CrudRepository<PhoneAuthDTO, String> {
-}
