a130c4a60d9f31fc674cc972b3c0939aa8c40798
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-05 10:46:38 +0900

    feat #197: 마감시간이 현재로부터 임계시간 지난 후보다 이전인 함께배달 select
    - 즉 마감이 임계시간 내 임박한 함께배달 경우 (e.g. 마감 5분 전)

--- src/main/java/fastcampus/team7/Livable_officener/repository/DeliveryRepository.java
+++ src/main/java/fastcampus/team7/Livable_officener/repository/DeliveryRepository.java
@@ -10,11 +10,13 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface DeliveryRepository extends JpaRepository<Room, Long>, DeliveryRepositoryCustom{
+public interface DeliveryRepository extends JpaRepository<Room, Long>, DeliveryRepositoryCustom {
 
     @Query("SELECT r FROM Room r JOIN RoomParticipant rp ON r.id = rp.room.id WHERE rp.user.id = :userId")
     Page<Room> findRoomsByUserId(@Param("userId") Long userId, Pageable pageable);
 
     @Query("SELECT r FROM Room r WHERE r.deadline > CURRENT_TIMESTAMP")
     List<Room> findByDeadlineAfterNow();
+
+    List<Room> findByDeadlineBefore(LocalDateTime afterLimitTime);
 }
