1569422e3e7d923d50ad1b002301ebba3e723fa7
서용현 <syh05247@naver.com>
2023-10-05 15:53:20 +0900

    feat #206: HTTPS 설정 확인 컨트롤러 및 cors 설정 추가

--- src/main/java/fastcampus/team7/Livable_officener/global/config/SecurityConfig.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/config/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://fc-chilli-bubble.github.io/front-officener:5173",
-                "https://fc-chilli-bubble.github.io/front-officener"));
+                "https://fc-chilli-bubble.github.io/front-officener",
+                "https://chillibubble-officener.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
